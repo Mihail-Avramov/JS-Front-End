c71168f72792253b8667b0a2de7f6b2256d857ed
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-07-04 11:56:12 +0300

    1
    1

--- 01-JS-Syntax-Fundamentals-Exercises/11-Road-Radar.js
+++ 01-JS-Syntax-Fundamentals-Exercises/11-Road-Radar.js
@@ -1,32 +1,29 @@
 function solve(speed, zone) {
     let zoneLimits = {
-        'city': 50,
-        'interstate': 90,
-        'motorway': 130,
-        'residential': 20,
-    }
+        city: 50,
+        interstate: 90,
+        motorway: 130,
+        residential: 20,
+    };
 
-    function status(speed, limit){
-        if(speed - limit > 40) {
-          return 'reckless driving'
-        }
-        else if (speed - limit > 20) {
-            return 'excessive speeding'
-        }
-        else {
-            return 'speeding'
+    function status(speed, limit) {
+        if (speed - limit > 40) {
+            return "reckless driving";
+        } else if (speed - limit > 20) {
+            return "excessive speeding";
+        } else {
+            return "speeding";
         }
     }
 
-    if (zoneLimits[zone] > speed){
-        console.log(`Driving ${speed} km/h in a ${zoneLimits[zone]} zone`)
-    }
-    else {
-        console.log(`The speed is ${speed - zoneLimits[zone]} km/h faster than the allowed speed of ${zoneLimits[zone]} - ${status(speed, zoneLimits[zone])}`)
+    if (zoneLimits[zone] >= speed) {
+        console.log(`Driving ${speed} km/h in a ${zoneLimits[zone]} zone`);
+    } else {
+        console.log(`The speed is ${speed - zoneLimits[zone]} km/h faster than the allowed speed of ${zoneLimits[zone]} - ${status(speed, zoneLimits[zone])}`);
     }
 }
 
-solve(40, 'city')
-solve(21, 'residential' )
-solve(120, 'interstate')
-solve(200, 'motorway')
+solve(40, "city");
+solve(21, "residential");
+solve(120, "interstate");
+solve(200, "motorway");
