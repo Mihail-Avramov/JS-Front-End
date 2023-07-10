0e323500741c62b912198c352466264333a30e06
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-07-10 10:18:19 +0300

    1
    1

--- 02-Functions-and-Statements-Exercises/06-Password-Validator.js
+++ 02-Functions-and-Statements-Exercises/06-Password-Validator.js
@@ -1,7 +1,26 @@
 function validatePassword(password) {
+    let passwordIsValid = true;
+
     if (password.length < 6 || password.length > 10) {
-        console.log;
+        console.log("Password must be between 6 and 10 characters");
+        passwordIsValid = false;
+    }
+
+    if (!password.match("^[A-Za-z0-9]+$")) {
+        console.log("Password must consist only of letters and digits");
+        passwordIsValid = false;
+    }
+
+    const digitCount = password.match(/\d/g);
+
+    if (!digitCount || digitCount.length < 2) {
+        console.log("Password must have at least 2 digits");
+        passwordIsValid = false;
+    }
+
+    if (passwordIsValid) {
+        console.log("Password is valid");
     }
 }
 
-validatePassword("MyPass123");
+validatePassword("Pa$s$s");
