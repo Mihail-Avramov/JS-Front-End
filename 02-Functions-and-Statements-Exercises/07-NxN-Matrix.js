b7445a77b4a3c80fe6499acee57f0770f49717f4
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-07-10 11:02:51 +0300

    3
    3

--- 02-Functions-and-Statements-Exercises/07-NxN-Matrix.js
+++ 02-Functions-and-Statements-Exercises/07-NxN-Matrix.js
@@ -1,6 +1,11 @@
 function solve(number) {
+
+    function printLine(number) {
+        console.log(`${number} `.repeat(number).trimEnd());  
+    }
+
     for (let index = 0; index < number; index++) {
-        console.log(`${number} `.repeat(number).trimEnd());        
+        printLine(number);     
     }
 }
 
