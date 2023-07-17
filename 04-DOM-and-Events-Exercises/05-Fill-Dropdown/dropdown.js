baf7a85596021fe64fc3ac5cc789076df7da85ac
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-07-17 13:35:32 +0300

    3
    3

--- 04-DOM-and-Events-Exercises/05-Fill-Dropdown/dropdown.js
+++ 04-DOM-and-Events-Exercises/05-Fill-Dropdown/dropdown.js
@@ -1,3 +1,18 @@
 function addItem() {
-    console.log('TODO:...');
+    const newItemTextInput = document.querySelector("#newItemText");
+    const newItemValueInput = document.querySelector("#newItemValue");
+
+    if (newItemTextInput.value === "" || newItemValueInput.value === "") {
+        return;
+    }
+
+    let select = document.querySelector("#menu");
+    let option = document.createElement("option");
+    option.text = newItemTextInput.value;
+    option.value = newItemValueInput.value;
+    select.appendChild(option);
+
+    newItemTextInput.value = "";
+    newItemValueInput.value = "";
+    newItemTextInput.focus();
 }
