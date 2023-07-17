9601be2e51dea1b630f70617c650ef3295bfe84f
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-07-17 16:59:51 +0300

    5
    5

--- 04-DOM-and-Events-Exercises/07-Format-the-Text/solution.js
+++ 04-DOM-and-Events-Exercises/07-Format-the-Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+    const input = document
+        .querySelector("#input")
+        .value.split(".")
+        .filter((element) => element && element !== "\n" && element.length > 1)
+        .map((element) => (element = element.trim() + "."));
+
+    console.log(input);
+
+    const container = document.querySelector("#output");
+
+    while (input.length > 0) {
+        const p = document.createElement("p");
+        p.textContent = input.splice(0, 3).join(" ");
+        container.appendChild(p);
+    }
 }
