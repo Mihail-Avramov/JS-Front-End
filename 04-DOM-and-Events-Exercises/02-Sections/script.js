d44735bd142516d3888fe44a7bd80f3e35d39b3f
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-07-17 12:12:11 +0300

    2
    2

--- 04-DOM-and-Events-Exercises/02-Sections/script.js
+++ 04-DOM-and-Events-Exercises/02-Sections/script.js
@@ -1,3 +1,16 @@
 function create(words) {
-   console.log('TODO:...');
+    const divContent = document.querySelector("#content");
+    words.forEach((word) => {
+        let newDiv = document.createElement("div");
+        let newParagraph = document.createElement("p");
+        newParagraph.textContent = word;
+        newParagraph.style.display = "none";
+        newDiv.appendChild(newParagraph);
+        newDiv.addEventListener("click", clickHandler);
+        divContent.appendChild(newDiv);
+    });
+
+    function clickHandler(e) {
+        e.currentTarget.querySelector("p").style.display = "block";
+    }
 }
--- 04-DOM-and-Events-Exercises/03-Accordion/accordion.js
+++ 04-DOM-and-Events-Exercises/03-Accordion/accordion.js
@@ -1,3 +1,20 @@
 function toggle() {
-    console.log('TODO:...');
+    const btn = document.querySelector("span.button");
+    const content = document.querySelector("#extra");
+
+    if (btn.textContent === "More") {
+        content.style.display = "block";
+        btn.textContent = "Less";
+    } else {
+        content.style.display = "none";
+        btn.textContent = "More";
+    }
 }
+
+// function toggle() {
+//     const content = document.querySelector("#extra");
+//     const button = document.querySelector("span.button");
+
+//     content.style.display = content.style.display === "block" ? "none" : "block";
+//     button.textContent = button.textContent === "More" ? "Less" : "More";
+// }
