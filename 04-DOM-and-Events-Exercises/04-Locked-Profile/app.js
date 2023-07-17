ab23ad63459db5334a00a81ec78662f8974e44fc
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-07-17 13:14:05 +0300

    3
    3

--- 04-DOM-and-Events-Exercises/04-Locked-Profile/app.js
+++ 04-DOM-and-Events-Exercises/04-Locked-Profile/app.js
@@ -2,12 +2,17 @@ function lockedProfile() {
     let buttons = Array.from(document.querySelectorAll("button"));
 
     buttons.forEach((button) => {
-        button.addEventListener = ("click", clickHandler);
+        button.addEventListener("click", () => {
+            if (button.parentElement.querySelector("input").checked) {
+                return;
+            }
+            if (button.innerText === "Show more") {
+                button.innerText = "Hide it";
+                button.parentElement.querySelector("div").style.display = "block";
+            } else {
+                button.innerText = "Show more";
+                button.parentElement.querySelector("div").style.display = "none";
+            }
+        });
     });
-
-    function clickHandler(e) {
-        e.cu
-    }
-
-    console.log(buttons);
 }
