752a6e6cc72b64a6c2af0bfc8dec074ff28bac6f
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-07-17 16:06:45 +0300

    4
    4

--- 04-DOM-and-Events-Exercises/06-Table-Search-Engine/solution.js
+++ 04-DOM-and-Events-Exercises/06-Table-Search-Engine/solution.js
@@ -1,8 +1,33 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+    document.querySelector("#searchField").addEventListener("keydown", function (e) {
+        console.log(e.code);
+        if (e.code === "Enter" || e.code === "NumpadEnter") {
+            onClick();
+        }
+    });
 
-   function onClick() {
-      //   TODO:
+    document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   }
+    function onClick() {
+        const input = document.querySelector("#searchField");
+        const cells = Array.from(document.querySelectorAll("tbody td"));
+        const selectedRows = Array.from(document.querySelectorAll("tbody tr.select"));
+
+        selectedRows.forEach((row) => {
+            row.classList.remove("select");
+        });
+
+        if (input.value === "") {
+            return;
+        }
+
+        cells.forEach((cell) => {
+            if (cell.textContent.includes(input.value)) {
+                cell.parentElement.classList.add("select");
+            }
+        });
+
+        input.value = "";
+        input.focus();
+    }
 }
