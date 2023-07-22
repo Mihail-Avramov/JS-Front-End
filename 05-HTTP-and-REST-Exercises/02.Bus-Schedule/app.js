fa586ded3e0ea02843dfc4178832149164d26513
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-07-22 19:24:15 +0300

    3
    3

--- 05-HTTP-and-REST-Exercises/02.Bus-Schedule/app.js
+++ 05-HTTP-and-REST-Exercises/02.Bus-Schedule/app.js
@@ -1,15 +1,36 @@
 function solve() {
+    const API_URL = "http://localhost:3030/jsonstore/bus/schedule/";
+    const departButton = document.getElementById("depart");
+    const arriveButton = document.getElementById("arrive");
+    const spanInfo = document.querySelector("#info > span");
+
+    let stopInfo = { name: "", next: "depot" };
+
     function depart() {
-        // TODO: 
+        fetch(API_URL + stopInfo.next)
+            .then((res) => res.json())
+            .then((data) => {
+                stopInfo = data;
+                spanInfo.textContent = `Next stop ${stopInfo.name}`;
+                departButton.disabled = true;
+                arriveButton.disabled = false;
+            })
+            .catch((err) => {
+                spanInfo.textContent = "Error";
+                departButton.disabled = true;
+                arriveButton.disabled = true;
+            });
     }
 
     async function arrive() {
-        // TODO:
+        spanInfo.textContent = `Arriving at ${stopInfo.name}`;
+        departButton.disabled = false;
+        arriveButton.disabled = true;
     }
 
     return {
         depart,
-        arrive
+        arrive,
     };
 }
 
