96c71a41ea9ad7b6dd05841f6ae9b3638f1406c3
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-07-24 13:01:09 +0300

    1
    1

--- 05-HTTP-and-REST-Exercises/04.Blog/app.js
+++ 05-HTTP-and-REST-Exercises/04.Blog/app.js
@@ -1,5 +1,57 @@
 function attachEvents() {
-    // TODO:
+    const POSTS_URL = "http://localhost:3030/jsonstore/blog/posts";
+    const COMMENTS_URL = "http://localhost:3030/jsonstore/blog/comments";
+
+    const btnLoadPosts = document.getElementById("btnLoadPosts");
+    const posts = document.getElementById("posts");
+    const btnViewPost = document.getElementById("btnViewPost");
+    const postTitle = document.getElementById("post-title");
+    const postBody = document.getElementById("post-body");
+    const postComments = document.getElementById("post-comments");
+
+    btnLoadPosts.addEventListener("click", loadPosts);
+    btnViewPost.addEventListener("click", viewPost);
+
+    let postsBodyData = {};
+
+    function loadPosts() {
+        fetch(POSTS_URL)
+            .then((res) => res.json())
+            .then((postsData) => {
+                Object.entries(postsData).forEach(([key, { body, _id, title }]) => {
+                    const optionElement = document.createElement("option");
+                    optionElement.value = key;
+                    optionElement.textContent = title;
+                    posts.appendChild(optionElement);
+                    postsBodyData[key] = body;
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    function viewPost() {
+        const postId = posts.value;
+        postTitle.textContent = posts.options[posts.selectedIndex].text;
+        postBody.textContent = postsBodyData[postId];
+        postComments.innerHTML = "";
+
+        fetch(COMMENTS_URL)
+            .then((res) => res.json())
+            .then((commentsData) => {
+                Object.values(commentsData)
+                    .filter((v) => v.postId === postId)
+                    .forEach((comment) => {
+                        const li = document.createElement("li");
+                        li.textContent = comment.text;
+                        postComments.appendChild(li);
+                    });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
 }
 
 attachEvents();
