dd2bed7a893e8a417ed805f7c6da9a545e968ef2
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-07-30 18:07:25 +0300

    3
    3

--- Exam-Preparation/Retake-Exam-20-Apr-2023/01.Horse-Racing/01.Horse-Racing.js
+++ Exam-Preparation/Retake-Exam-20-Apr-2023/01.Horse-Racing/01.Horse-Racing.js
@@ -1,1 +1,69 @@
-//TODO...
+function solve(input) {
+    const horsesRanking = input.shift().split("|");
+    let commandInput = "";
+    while ((commandInput = input.shift()) !== "Finish") {
+        const [command, firstHorese, secondHorse] = commandInput.split(" ");
+        const firstHoreseIndex = horsesRanking.indexOf(firstHorese);
+        switch (command) {
+            case "Retake":
+                let overtakingHoreseIndex = horsesRanking.indexOf(firstHorese);
+                let overtakenHoreseIndex = horsesRanking.indexOf(secondHorse);
+                if (overtakingHoreseIndex < overtakenHoreseIndex) {
+                    [
+                        horsesRanking[overtakingHoreseIndex],
+                        horsesRanking[overtakenHoreseIndex],
+                    ] = [
+                        horsesRanking[overtakenHoreseIndex],
+                        horsesRanking[overtakingHoreseIndex],
+                    ];
+                    console.log(`${firstHorese} retakes ${secondHorse}.`);
+                }
+                break;
+            case "Trouble":
+                if (firstHoreseIndex > 0) {
+                    [
+                        horsesRanking[firstHoreseIndex],
+                        horsesRanking[firstHoreseIndex - 1],
+                    ] = [
+                        horsesRanking[firstHoreseIndex - 1],
+                        horsesRanking[firstHoreseIndex],
+                    ];
+                    console.log(
+                        `Trouble for ${firstHorese} - drops one position.`
+                    );
+                }
+                break;
+            case "Rage":
+                let toPositionIndex = null;
+                if (firstHoreseIndex < horsesRanking.length - 2) {
+                    toPositionIndex = firstHoreseIndex + 2;
+                } else if (firstHoreseIndex < horsesRanking.length - 1) {
+                    toPositionIndex = firstHoreseIndex + 1;
+                } else {
+                    toPositionIndex = firstHoreseIndex;
+                }
+
+                horsesRanking.splice(firstHoreseIndex, 1);
+                horsesRanking.splice(toPositionIndex, 0, firstHorese);
+
+                console.log(`${firstHorese} rages 2 positions ahead.`);
+                break;
+            case "Miracle":
+                const horseName = horsesRanking.shift();
+                horsesRanking.push(horseName);
+                console.log(`What a miracle - ${horseName} becomes first.`);
+                break;
+        }
+    }
+    console.log(horsesRanking.join("->"));
+    console.log(`The winner is: ${horsesRanking.pop()}`);
+}
+
+solve([
+    "Onyx|Domino|Sugar|Fiona",
+    "Trouble Onyx",
+    "Retake Onyx Sugar",
+    "Rage Domino",
+    "Miracle",
+    "Finish",
+]);
--- Exam-Preparation/Retake-Exam-20-Apr-2023/02.Post-Task/app.js
+++ Exam-Preparation/Retake-Exam-20-Apr-2023/02.Post-Task/app.js
@@ -1,5 +1,104 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  
+    const formTaskInputs = {
+        title: document.getElementById("task-title"),
+        category: document.getElementById("task-category"),
+        content: document.getElementById("task-content"),
+    };
+
+    const reviewList = document.getElementById("review-list");
+    const publishedList = document.getElementById("published-list");
+    const formTask = document.querySelector("#newPost > form");
+    const publishBtn = document.getElementById("publish-btn");
+
+    publishBtn.addEventListener("click", publishTaskHandler);
+
+    function publishTaskHandler() {
+        if (Object.values(formTaskInputs).some((input) => input.value === "")) {
+            return;
+        }
+
+        const [title, category, content] = Object.values(formTaskInputs).map(
+            (input) => input.value
+        );
+
+        const reviewLi = generateElement("li", reviewList, null, null, [
+            "rpost",
+        ]);
+        const reviewArticle = generateElement("article", reviewLi);
+        generateElement("h4", reviewArticle, title);
+        generateElement("p", reviewArticle, `Category: ${category}`);
+        generateElement("p", reviewArticle, `Content: ${content}`);
+        const btnEdit = generateElement("button", reviewLi, "Edit", null, [
+            "action-btn",
+            "edit",
+        ]);
+        const btnPost = generateElement("button", reviewLi, "Post", null, [
+            "action-btn",
+            "post",
+        ]);
+
+        btnEdit.addEventListener("click", () => {
+            formTaskInputs.title.value = title;
+            formTaskInputs.category.value = category;
+            formTaskInputs.content.value = content;
+            reviewLi.remove();
+        });
+
+        btnPost.addEventListener("click", () => {
+            publishedList.appendChild(reviewLi);
+            btnEdit.remove();
+            btnPost.remove();
+        });
+
+        formTask.reset();
+    }
+
+    function generateElement(
+        type,
+        parent,
+        textContent,
+        id,
+        classesArray,
+        attributesObj
+    ) {
+        //"HTML-TAG"
+        const element = document.createElement(type);
+
+        //"text or value content"
+        if (textContent) {
+            if (type !== "input" && type !== "textarea") {
+                element.textContent = textContent;
+            } else {
+                element.value = textContent;
+            }
+        }
+
+        // "idName"
+        if (id) {
+            element.id = id;
+        }
+
+        // ["className1", "className2", ...]
+        if (classesArray) {
+            classesArray.forEach((className) => {
+                element.classList.add(className);
+            });
+        }
+
+        //{src: "link to img", href: "link to site", type: "checkbox", row: 20}
+        if (attributesObj) {
+            Object.entries(attributesObj).forEach(([key, value]) => {
+                element.setAttribute(key, value);
+            });
+        }
+
+        // append HTMLelement to HTMLparent
+        if (parent) {
+            parent.appendChild(element);
+        }
+
+        return element;
+    }
 }
